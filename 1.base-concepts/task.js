45da74d36197ba08e2554c998dccec37da91c293
Игорь Лухт <9425185@mail.ru>
2023-04-06 00:24:52 +0300

    продолжение решения задачи 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,8 +2,14 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let discr = b ** 2 - 4 * a * c;
-    if (discr == 0) {
-
+    if (discr === 0) {
+      let x = -b / (2 * a);
+      arr.push(x);
+    } 
+    if (discr > 0){
+      let x1 = (-b + Math.sqrt(d) )/(2*a);
+      let x2 = (-b - Math.sqrt(d) )/(2*a);
+      arr.push(x1, x2);
     }
   return arr;
 }
