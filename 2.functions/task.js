3bac7185d00caeb559018e15a2d13f7affb27e47
Игорь Лухт <9425185@mail.ru>
2023-04-07 17:00:14 +0300

    начал решать 1 задачу

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,19 @@
 function getArrayParams(...arr) {
-
+    let min = arr[0];
+    let max = arr[0];
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        
+    }
+      
+    
   return { min: min, max: max, avg: avg };
 }
+
+
+
+
+
 
 function summElementsWorker(...arr) {
 
