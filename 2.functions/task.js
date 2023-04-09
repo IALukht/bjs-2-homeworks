1afa3e13778e27e9fbdf18635c04bc0faef8c6d5
Игорь Лухт <9425185@mail.ru>
2023-04-09 22:03:14 +0300

    решил 2 задачу функций

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,7 @@
 function getArrayParams(...arr) {
+  if (arr.length === 0){
+    return 0;
+  }  
     let min = arr[0];
     let max = arr[0];
     let sum = 0;
@@ -22,19 +25,51 @@ function getArrayParams(...arr) {
 
 
 function summElementsWorker(...arr) {
-
+  if (arr.length === 0){
+    return 0;
+  }
+  let sum = 0;
+  for (i = 0; i < arr.length; i++){
+  sum += arr[i];
+  }
+  return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
+    if (arr.length === 0){
+      return 0;
+    }
+    return Math.max(...arr) - Math.min(...arr);
+  }
 
-}
 
 function differenceEvenOddWorker(...arr) {
-
+  if (arr.length === 0){
+    return 0}
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  for (i = 0; i < arr.length; i++){
+    if (arr[i] % 2 == 0){
+      sumEvenElement += arr[i];   
+    } else {
+      sumOddElement += arr[i];
+    }
+  }
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  if (arr.length === 0){
+    return 0}
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  for (i = 0; i < arr.length; i++){
+    if (arr[i] % 2 == 0){
+      sumEvenElement += arr[i];
+      countEvenElement += 1;   
+    }
+  }
+  return sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
