ddbbba0c81171fb5ec521e1ceb6793b709ee2b8c
Игорь Лухт <9425185@mail.ru>
2023-04-09 19:35:45 +0300

    решил 1 задачу

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -3,9 +3,15 @@ function getArrayParams(...arr) {
     let max = arr[0];
     let sum = 0;
     for (let i = 0; i < arr.length; i++){
-        
+      if (arr[i] < min){
+        min = arr[i];
+      }
+      if (arr[i] > max){
+        max = arr[i];
+      };
+        sum += arr[i];
     }
-      
+     let avg = +(sum / arr.length).toFixed(2); 
     
   return { min: min, max: max, avg: avg };
 }
