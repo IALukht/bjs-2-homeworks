c7ab72a47a5c250ce8deda088acbb52e8a46fa03
Игорь Лухт <9425185@mail.ru>
2023-04-11 22:46:46 +0300

    решение 1 задачи по массивам

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -74,7 +74,7 @@ function averageEvenElementsWorker(...arr) {
 
 function makeWork (arrOfArr, func) {
   let maxWorkerResult = -Infinity;
-  for (let i = 0; i < arrOfArr.length; i++){
+  for ( i = 0; i < arrOfArr.length; i++){
     const result = func(...arrOfArr[i]);
     if (result >  maxWorkerResult){
       maxWorkerResult = result;
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,4 @@
-function compareArrays(arr1, arr2) {
-  
-}
+let compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((value, i) => value === arr2[i]) 
 
 function getUsersNamesInAgeRange(users, gender) {
   
