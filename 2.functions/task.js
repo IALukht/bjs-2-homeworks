4154822dad74d4bd8ceee62bef91d0f6afe43d7c
Игорь Лухт <9425185@mail.ru>
2023-04-11 22:51:18 +0300

    объявил index

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -74,7 +74,7 @@ function averageEvenElementsWorker(...arr) {
 
 function makeWork (arrOfArr, func) {
   let maxWorkerResult = -Infinity;
-  for ( i = 0; i < arrOfArr.length; i++){
+  for ( let i = 0; i < arrOfArr.length; i++){
     const result = func(...arrOfArr[i]);
     if (result >  maxWorkerResult){
       maxWorkerResult = result;
