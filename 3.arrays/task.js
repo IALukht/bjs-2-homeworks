84f0ed89257cd4e961c57e1542ac2ec22d9fd126
Игорь Лухт <9425185@mail.ru>
2023-04-13 21:37:36 +0300

    изменил решение 2 задачи

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,5 @@
 let compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((value, i) => value === arr2[i]);
 
 function getUsersNamesInAgeRange(users, gender) {
- return users.filter(elementGender => gender === elementGender.gender).
- map(AgeGender => AgeGender.age).
- reduce((summator, age, index, userAges ) => summator + age / userAges.length, 0)
+    return users.filter(elementGender => gender === elementGender.gender).map(AgeGender => AgeGender.age).reduce((summator, age, i, userAges) => summator + age / userAges.length, 0)
 }
