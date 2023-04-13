aa52666161577d40babd613a5d767ee0732d6b8d
Игорь Лухт <9425185@mail.ru>
2023-04-13 18:22:20 +0300

    решение 2 задачи_массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,7 @@
-let compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((value, i) => value === arr2[i]) 
+let compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((value, i) => value === arr2[i]);
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+ return users.filter(elementGender => gender === elementGender.gender).
+ map(AgeGender => AgeGender.age).
+ reduce((summator, age, index, userAges ) => summator + age / userAges.length, 0)
 }
