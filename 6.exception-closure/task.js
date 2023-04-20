c7909c72ac20f5259b21b03fba0ee737f15a008d
Игорь Лухт <9425185@mail.ru>
2023-04-20 22:31:19 +0300

    решение 1 и 2 задачи

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,53 @@
-﻿
+﻿function parseCount (number){
+  let result = Number.parseFloat(number);
+    if (isNaN(result)){
+      throw new Error("Невалидное значение");
+    }
+    return result;
+}
+
+function validateCount(number){
+    try {
+      return parseCount (number);
+    } catch (error){
+    return error;
+    }
+}
+
+// Задача 2
+
+class Triangle{
+  constructor (a, b, c){
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    if (a + b < c || b + c < a || a + c < b) {
+        throw new Error("Треугольник с такими сторонами не существует"); 
+    }
+  }
+  
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area(){
+    const p = (this.a + this.b + this.c) / 2;
+    return +(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+  }
+}
+
+  function getTriangle(a, b, c) {
+    try {
+      return new Triangle(a, b, c);
+    } catch (err) {
+      return {
+        get area() {
+            return "Ошибка! Треугольник не существует";
+        },
+        get perimeter() {
+            return "Ошибка! Треугольник не существует"
+        }
+      }
+    }
+}
